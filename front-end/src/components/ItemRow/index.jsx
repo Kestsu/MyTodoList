f6d6c1b7db3af4e0a91ace7fb7d4f160fb9953aa
Kevin Li <Kevinstart14@gmail.com>
2023-04-14 18:04:48 -0300

    ajustado o input de edição

--- front-end/src/components/ItemRow/index.jsx
+++ front-end/src/components/ItemRow/index.jsx
@@ -22,48 +22,50 @@ function ItemRow({ index, id, description, check }) {
       <div className="item-row">
         {editMode ? (
           <>
-            <div className="item-row-desc">
-              <input
-                data-testid={`todo-task-edit-input-${index}`}
-                value={inputDescription}
-                onChange={editHandle}
-              />
-            </div>
-            <div>
-              <button
-                data-testid={`todo-task-edit-save-btn-${index}`}
-                style={{ marginRight: "5px" }}
-                onClick={editSave}
-              >
-                <FaCheck />
-              </button>
-              <button
-                data-testid={`todo-task-edit-cancel-btn-${index}`}
-                onClick={() => setEditMode(false)}
-              >
-                <FaRegWindowClose />
-              </button>
+            <div className="container-edit">
+              <div className="item-row-desc">
+                <input
+                  data-testid={`todo-task-edit-input-${index}`}
+                  className="input-edit"
+                  value={inputDescription}
+                  onChange={editHandle}
+                />
+              </div>
+              <div className="container-button-edit">
+                <button
+                  data-testid={`todo-task-edit-save-btn-${index}`}
+                  style={{ marginRight: "5px", marginTop: "5px" , marginBottom: "5px" }}
+                  onClick={editSave}
+                >
+                  <FaCheck />
+                </button>
+                <button
+                  data-testid={`todo-task-edit-cancel-btn-${index}`}
+                  style={{ marginRight: "5px", marginTop: "5px" , marginBottom: "5px" }}
+                  onClick={() => setEditMode(false)}
+                >
+                  <FaRegWindowClose />
+                </button>
+              </div>
             </div>
           </>
         ) : (
           <>
-            {/* <div class="container"> */}
-              <input 
+            <input
               data-testid={`todo-task-check-input-${index}`}
-              type="checkbox" 
-              id="cbx2" 
+              type="checkbox"
+              id="cbx2"
               className="scales"
               name="scales"
               defaultChecked={check}
               onChange={changeCheck}
-              />
-              <label htmlFor="cbx2" className="check">
-                <svg width="18px" height="18px" viewBox="0 0 18 18">
-                  <path d="M 1 9 L 1 9 c 0 -5 3 -8 8 -8 L 9 1 C 14 1 17 5 17 9 L 17 9 c 0 4 -4 8 -8 8 L 9 17 C 5 17 1 14 1 9 L 1 9 Z"></path>
-                  <polyline points="1 9 7 14 15 4"></polyline>
-                </svg>
-              </label>
-            {/* </div> */}
+            />
+            <label htmlFor="cbx2" className="check">
+              <svg width="18px" height="18px" viewBox="0 0 18 18">
+                <path d="M 1 9 L 1 9 c 0 -5 3 -8 8 -8 L 9 1 C 14 1 17 5 17 9 L 17 9 c 0 4 -4 8 -8 8 L 9 17 C 5 17 1 14 1 9 L 1 9 Z"></path>
+                <polyline points="1 9 7 14 15 4"></polyline>
+              </svg>
+            </label>
 
             <div
               className={clsx("item-row-desc", check && "item-row-desc-check")}
@@ -73,13 +75,14 @@ function ItemRow({ index, id, description, check }) {
             <div style={{ width: "66px" }}>
               <button
                 data-testid={`todo-task-edit-mode-btn-${index}`}
-                style={{ marginRight: "5px" }}
+                style={{ marginRight: "5px", marginTop: "5px" , marginBottom: "5px" }}
                 onClick={() => setEditMode(true)}
               >
                 <FaEdit />
               </button>
               <button
                 data-testid={`todo-task-remove-btn-${index}`}
+                style={{ marginRight: "5px", marginTop: "5px" , marginBottom: "5px" }}
                 onClick={() => rmTask(id)}
               >
                 <FaRegWindowClose />
