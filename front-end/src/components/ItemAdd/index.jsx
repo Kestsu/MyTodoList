b8e93c8bfa6426acc64a88f234e7c870b2f052fa
Kevin Li <Kevinstart14@gmail.com>
2023-04-14 01:25:51 -0300

    ajustado o input de nova tarefa

--- front-end/src/components/ItemAdd/index.jsx
+++ front-end/src/components/ItemAdd/index.jsx
@@ -1,7 +1,7 @@
-import React, { useContext, useState } from 'react';
-import './styles.css';
-import { FaPlus } from 'react-icons/fa';
-import TaskContext from '../../context/taskContext';
+import React, { useContext, useState } from "react";
+import "./styles.css";
+import { FaPlus } from "react-icons/fa";
+import TaskContext from "../../context/taskContext";
 
 function ItemAdd() {
   const [description, setDescription] = useState("");
@@ -11,20 +11,31 @@ function ItemAdd() {
 
   return (
     <div className="item-add">
-      <div>
-        <label>Nova tarefa:</label>
+      <div className="form-control">
         <input
-          data-testid="todo-task-input" 
-          value={description} 
-          onChange={
-            ({target: { value }}) => setDescription(value)
-          } 
+          type="text"
+          required
+          data-testid="todo-task-input"
+          value={description}
+          onChange={({ target: { value } }) => setDescription(value)}
         />
+        <label>
+          <span style={{ transitionDelay: "0ms" }}>N</span>
+          <span style={{ transitionDelay: "50ms" }}>o</span>
+          <span style={{ transitionDelay: "100ms" }}>v</span>
+          <span style={{ transitionDelay: "150ms" }}>a</span>
+          <span style={{ transitionDelay: "200ms" }}> </span>
+          <span style={{ transitionDelay: "250ms" }}>T</span>
+          <span style={{ transitionDelay: "300ms" }}>a</span>
+          <span style={{ transitionDelay: "350ms" }}>r</span>
+          <span style={{ transitionDelay: "400ms" }}>e</span>
+          <span style={{ transitionDelay: "450ms" }}>f</span>
+          <span style={{ transitionDelay: "500ms" }}>a</span>
+        </label>
       </div>
-      <button
-        data-testid="todo-task-add"
-        onClick={handleAdd}
-      ><FaPlus /></button>
+      <button data-testid="todo-task-add" onClick={handleAdd}>
+        <FaPlus />
+      </button>
     </div>
   );
 }
