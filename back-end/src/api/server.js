9b10487f24fe69bc4faf2aab85e3f741d7c16dcd
Kevin Li <Kevinstart14@gmail.com>
2023-04-14 23:40:04 -0300

    alterado a mensagem da porta back-end

--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -2,4 +2,4 @@ const server = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 
-server.listen(PORT, () => console.log("'Trybe Todo List Api' running on port", PORT));
+server.listen(PORT, () => console.log("'Todo List Api' running on port", PORT));
